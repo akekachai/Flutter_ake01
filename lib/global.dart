39e71d6f655a31974a4910ebb9a86eb43cb59e32
akekachai <akekachaiaj@gmail.com>
2021-07-19 09:23:16 +0700

    initial commit

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,12 @@
 library flutter_app02.globals;
 
-String urlbaseMember =
-    'https://script.google.com/macros/s/AKfycbzvp5TIZmHJrn1oggzz5ih-S8VgwHuHh2d0iWmHtncnfaPKKGQg79U9_uqFG6z7GS96hQ/exec?path=/apiv&query=';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class Global {
+  static const Color white = const Color(0xffffffff);
+  static const Color mediumBlue = const Color(0xff4A64FE);
+  static const List validEmail = ['test@gmail.com'];
+  String urlbaseMember =
+      'https://script.google.com/macros/s/AKfycbzvp5TIZmHJrn1oggzz5ih-S8VgwHuHh2d0iWmHtncnfaPKKGQg79U9_uqFG6z7GS96hQ/exec?path=/apiv&query=';
+}
